43a745ee8128ce9d0cea2a9d9c68430444914dd1
nn <abdmoeed686@gmail.com>
2026-05-11 09:52:42 +0500

    anything

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const userProfileRoutes = require("./routes/userSellListRoutes"); // Reviews and
 const userSellListRoutes = require("./routes/user"); // The actual sell list
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);});
 
